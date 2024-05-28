9f4efaf56b2a62dc4692458617f41b8b2f6c6d21
Paul Ndambo <164306210+ndambopaul@users.noreply.github.com>
2024-05-28 10:07:31 +0300

    booking update and delete added

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const { getBookings, getBookingById, createBooking } = require("../controllers/bookings");
+const { getBookings, getBookingById, createBooking, updateBooking, deleteBooking } = require("../controllers/bookings");
 
 const router = express.Router();
 
 router.get("/", getBookings);
 router.get("/:id", getBookingById);
 router.post("/", createBooking);
+router.put("/:id", updateBooking);
+router.delete("/:id", deleteBooking);
 
 module.exports = router
