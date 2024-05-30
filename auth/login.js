8a82ced89de34baa411ebfd00337df7c31278976
Paul Ndambo <164306210+ndambopaul@users.noreply.github.com>
2024-05-30 11:17:14 +0300

    backend fixes

--- auth/login.js
+++ auth/login.js
@@ -14,7 +14,7 @@ const login = async(req, res) => {
         const passwordMatches = await bcrypt.compare(password, user.password);
         if(!passwordMatches) return res.status(400).send({ error: "Incorrect Password!!" });
 
-        const token = await jwt.sign({ user: { id: user.id, email: user.email, username: user.username, first_name: user.first_name, last_name: user.last_name, phone_number: user.phone_number } }, "1234", {expiresIn: "1h"})
+        const token = await jwt.sign({ user: { id: user.id, email: user.email, username: user.username, first_name: user.first_name, last_name: user.last_name, phone_number: user.phone_number } }, "1234", {expiresIn: "25h"})
         res.send({ token }).status(200)
 
     } catch (error) {
